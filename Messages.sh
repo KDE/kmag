82a75735792c71f8ac467ea3eb81f0c710e22ec2
Laurent Montel <montel@kde.org>
2007-08-09 20:47:18 +0000

    Not necessary to complexify it
    svn path=/trunk/KDE/kdeaccessibility/kmag/; revision=698322

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,2 @@
 #! /usr/bin/env bash
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
-if test -n "$LIST"; then 
-	$XGETTEXT $LIST -o $podir/kmag.pot
-fi
+$XGETTEXT *.cpp -o $podir/kmag.pot
